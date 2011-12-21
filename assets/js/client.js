b07f4a901d83ae81f937324c8392415a1bf39c94
David <david@hermes.(none)>
2011-12-21 12:57:18 -0800

    Fixing js reference

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,6 +1,6 @@
 //= require 'libs/socket.io.js'
 //= require 'libs/jquery-1.7.1.min.js'
-//= require 'libs/jquery.scrollTo-1.4.2.min.js'
+//= require 'libs/jquery.scrollTo-1.4.2-min.js'
 //= require 'libs/underscore-min.js'
 //= require 'libs/backbone-min.js'
 //= require 'libs/ICanHaz.min.js'
