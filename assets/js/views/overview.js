f948bc9c4a4c47245e39b23f7f4c3e4d73bf19e6
David <david@hermes.(none)>
2011-12-27 06:57:50 -0800

    Basic Navigation from overview menu

--- assets/js/views/overview.js
+++ assets/js/views/overview.js
@@ -7,8 +7,15 @@ var OverViewView = Backbone.View.extend({
 
   className: 'container-fluid',
 
-  render: function() {
+  render: function(event) {
     $('.content').html($(this.el).html(ich.overview()));
+    if(event === undefined) {
+      $('#overview').html(ich.overview_home());   
+    } else {
+      var func = ich['overview_' + event.currentTarget.id];  
+      $('#overview').html(func());
+    }
+    $('.overview_button').bind('click', jQuery.proxy(this.render, this));
     return this;
   }
 });
