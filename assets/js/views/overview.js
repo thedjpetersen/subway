390af2f63338fc75f63fef7d7f8b7806d10fa5b9
David <david@hermes.(none)>
2011-12-27 09:53:25 -0800

    Adding loading logic

--- assets/js/views/overview.js
+++ assets/js/views/overview.js
@@ -16,6 +16,21 @@ var OverViewView = Backbone.View.extend({
       $('#overview').html(func());
     }
     $('.overview_button').bind('click', jQuery.proxy(this.render, this));
+    $('#connect_button').bind('click', function(){
+      $('.error').removeClass('error');
+      if(!$('#connect_server').val()) {
+        $('#connect_server').closest('.clearfix').addClass('error');
+        $('#connect_server').addClass('error');  
+      }
+      if(!$('#connect_nick').val()) {
+        $('#connect_nick').closest('.clearfix').addClass('error');
+        $('#connect_nick').addClass('error');  
+      } 
+      if($('#connect_nick').val() && $('#connect_server')) {
+        $('form').append(ich.load_image()); 
+        $('#connect_button').addClass('disabled');
+      }
+    });
     return this;
   }
 });
