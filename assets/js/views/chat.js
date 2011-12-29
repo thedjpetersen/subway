8dafc0ee18e119d25cd13d2dccfe28e48bfdc28a
David <david@hermes.(none)>
2011-12-29 14:01:28 -0800

    Fix capitalization

--- assets/js/views/chat.js
+++ assets/js/views/chat.js
@@ -6,7 +6,7 @@ var ChatView = Backbone.View.extend({
   render: function() {
     // $('.content').html(this.el);
     $('.content').html(ich.chat());
-    var activeChannel = window.irc.ChatWindows.getActive();
+    var activeChannel = window.irc.chatWindows.getActive();
     return this;
   }
 });
