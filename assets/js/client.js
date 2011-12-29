54210201062badf84f819de0d7fbd9cdb6e2272a
David <david@hermes.(none)>
2011-12-29 03:02:47 -0800

    Removing chat application model since that is not in the new models

--- assets/js/client.js
+++ assets/js/client.js
@@ -11,7 +11,6 @@
 var ChatApplicationRouter = Backbone.Router.extend({
   initialize: function(options) {
     this.socket = io.connect();
-    this.model = new ChatApplicationModel;
     this.view = new ChatApplicationView;
   }
 });
