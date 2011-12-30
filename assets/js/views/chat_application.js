5382c71d1e7b8560b3b990dfd417039f7c5d1fe7
Aaron Kavlie <akavlie@gmail.com>
2011-12-30 16:06:33 -0700

    Message adding working (for status window at least).

--- assets/js/views/chat_application.js
+++ assets/js/views/chat_application.js
@@ -1,6 +1,7 @@
 var ChatApplicationView = Backbone.View.extend({
   initialize: function() {
     this.render();
+    irc.chatWindows.bind('change:active', this.focus, this);
   },
 
   className: 'container-fluid',
@@ -14,5 +15,21 @@ var ChatApplicationView = Backbone.View.extend({
       var channelList = new ChannelListView;
     }
     return this;
+  },
+
+  addMessage: function(msg) {
+    var view = new MessageView({model: msg});
+    $('#chat-contents').append(view.el);
+    console.log('message added!');
+    $('#chat-contents').scrollTop( $('#chat-contents').scrollTop() + 100 );
+  },
+
+  focus: function(chat) {
+    if (!chat.get('active')) {
+      console.log(chat.get('name') + ' not set as active');
+      return;
+    }
+    console.log('focused on channel ' + chat.get('name'));
+    chat.stream.bind('add', this.addMessage, this);
   }
 });
