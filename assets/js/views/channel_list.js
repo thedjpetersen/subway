fe03916c181ae97606df4b901c4b63610fcb6870
Aaron Kavlie <akavlie@gmail.com>
2012-01-14 13:14:30 -0700

    Add separate view for channel tabs.

--- assets/js/views/channel_list.js
+++ assets/js/views/channel_list.js
@@ -4,28 +4,27 @@ var ChannelListView = Backbone.View.extend({
   initialize: function() {
     console.log('channel list init');
     irc.chatWindows.bind('add', this.addChannel, this);
-    irc.chatWindows.bind('setActive', this.setActiveChannel, this);
   },
 
   addChannel: function(chat) {
     console.log('channel added to list');
-    chat.channelTab = ich.channel({name:chat.get('name')});
-    $(this.el).append(chat.channelTab);
-    this.setActiveChannel(chat);
+    // chat.channelTab = ich.channel({name:chat.get('name')});
+    var view = new ChannelTabView({model: chat})
+    $(this.el).append(view.render().el);
+    irc.chatWindows.setActive(chat);
     // TODO: simplify / rewrite this
     // separate view for each tab would probably simplify things
-    chat.channelTab.click({chat: chat, clv: this}, function(ev) {
-      chat = ev.data.chat;
-      irc.chatWindows.setActive(chat);
-      ev.data.clv.setActiveChannel(chat);
-    });
+    // chat.channelTab.click({chat: chat, clv: this}, function(ev) {
+    //   chat = ev.data.chat;
+    //   ev.data.clv.setActiveChannel(chat);
+    // });
   },
 
-  setActiveChannel: function(chat) {
-    $('.active').removeClass('active');
-    if (chat.channelTab !== undefined) {
-      chat.channelTab.addClass('active');
-    }
-  }
+  // setActiveChannel: function(chat) {
+  //   $('.active').removeClass('active');
+  //   if (chat.channelTab !== undefined) {
+  //     chat.channelTab.addClass('active');
+  //   }
+  // }
 
 });
