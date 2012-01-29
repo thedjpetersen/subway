a77d608745a18017949807792191760d40643f5b
Aaron Kavlie <akavlie@gmail.com>
2012-01-28 22:33:44 -0700

    Improve formatting of _formatDate.

--- assets/js/views/message.js
+++ assets/js/views/message.js
@@ -29,7 +29,7 @@ var MessageView = Backbone.View.extend({
     return this;
   },
 
-  _formatDate: function (date) {
+  _formatDate: function(date) {
     var d = new Date(date);
     var hh = d.getHours();
     var m = d.getMinutes();
@@ -37,24 +37,17 @@ var MessageView = Backbone.View.extend({
     var dd = "AM";
     var h = hh;
     if (h >= 12) {
-      h = hh-12;
+      h = hh - 12;
       dd = "PM";
     }
     if (h == 0) {
       h = 12;
     }
-    m = m<10?"0"+m:m;
 
-    s = s<10?"0"+s:s;
+    m = m < 10 ? "0" + m:m;
+    s = s < 10 ? "0" + s:s;
 
-    /* if you want 2 digit hours:
-    h = h<10?"0"+h:h; */
-
-    var replacement = h+":"+m;
-    /* if you want to add seconds
-    repalcement += ":"+s;  */
-    replacement += " "+dd;
-
+    var replacement = h + ":" + m + " " + dd;
     return d.toDateString() + ', ' + replacement;
   }
 
