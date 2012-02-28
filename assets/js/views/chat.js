0332bbeff67d7fe6b81e79a48bccf5f94c35c00b
David Petersen <david.petersen@emberex.com>
2012-02-27 21:39:32 -0800

    Inifinite scrolling for private messages

--- assets/js/views/chat.js
+++ assets/js/views/chat.js
@@ -171,7 +171,15 @@ var ChatView = Backbone.View.extend({
     $('#chat-contents').scroll(function(){
       if ($('#chat-contents').scrollTop() < 150) {
         var skip = (-50)-$('#chat-contents').children().length;
-        irc.socket.emit('getOldMessages',{channelName: irc.chatWindows.getActive().get('name'), skip:skip, amount: 10});
+        var windowName = irc.chatWindows.getActive().get('name');
+        var target;
+        if(windowName[0] == '#'){
+          target = windowName;
+        } else {
+          var userName = irc.me.get('nick');
+          target = (userName < windowName) ? userName + windowName : windowName + userName;
+        }
+        irc.socket.emit('getOldMessages',{channelName: target, skip:skip, amount: 10});
       }
     });
   },
