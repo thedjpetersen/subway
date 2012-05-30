d96ad89b126e93468134e706130330a89c87ae01
Daniel Insley <dan@pulsesoftware.ca>
2012-05-30 16:24:10 -0700

    fixed issue where it was always attempting to connect via SSL

--- assets/js/views/overview.js
+++ assets/js/views/overview.js
@@ -45,20 +45,24 @@ var OverviewView = Backbone.View.extend({
   connect: function(event) {
     event.preventDefault();
     $('.error').removeClass('error');
+
     var server = $('#connect-server').val();
     var nick = $('#connect-nick').val();
-		var port = $('#connect-port').val();
-		var secure = $('#connect-secure').val();
-		var selfSigned = $('#connect-selfSigned').val();
-		var password = $('#connect-password').val();
+    var port = $('#connect-port').val();
+    var secure = $('#connect-secure').is(':checked');
+    var selfSigned = $('#connect-selfSigned').is(':checked');
+    var password = $('#connect-password').val();
+    
     if (!server) {
       $('#connect-server').closest('.clearfix').addClass('error');
       $('#connect-server').addClass('error');
     }
+    
     if (!nick) {
       $('#connect-nick').closest('.clearfix').addClass('error');
       $('#connect-nick').addClass('error');
     }
+    
     if (nick && server) {
       $('form').append(ich.load_image());
       $('#connect-button').addClass('disabled');
@@ -66,10 +70,10 @@ var OverviewView = Backbone.View.extend({
       var connectInfo = {
         nick: nick,
         server: server,
-				port: port,
-				secure: secure,
-				selfSigned: selfSigned,
-				password: password
+	port: port,
+        secure: secure,
+        selfSigned: selfSigned,
+        password: password
       };
       irc.me = new User(connectInfo);
       irc.me.on('change:nick', irc.appView.renderUserBox);
