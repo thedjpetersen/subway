817a1b1c2eff2e7b2a2a244dd205e4ac40e2893c
Cameron Currie <me@cameroncurrie.net>
2012-05-30 18:16:50 +0200

    Move port setting into config.js

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
-exports.mongodb = {
+exports.dev = {
+    port: 3000,
+    mongoose_auth: 'mongodb://mongodb@localhost/subway'
+}
+
+exports.prod = {
+    port: 14858, // Nodester port
     mongoose_auth: 'mongodb://mongodb@localhost/subway'
 }
