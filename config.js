c2bb9d7e63c6b07880d7029918122591b23ef8ed
Hugues Obolonsky <hugh@atosc.org>
2013-01-01 01:22:53 +0000

    More generic config file

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ exports.dev = {
     port: 3000,
     client_port: 3000,
     mongoose_auth: 'mongodb://mongodb@localhost/subway',
-    default_ircserver: '192.168.56.54',
-    default_ircchannel: '#powerline'
+    // default_ircserver: '127.0.0.1',
+    // default_ircchannel: '#your_chan'
 }
 
 exports.prod = {
     port: 14858, // Nodester port
     client_port: 80, // Websockets talk on port 80 on Nodester, regardless of listen port
     mongoose_auth: 'mongodb://mongodb@localhost/subway',
-    default_ircserver: '192.168.56.54',
-    default_ircchannel: '#powerline'
+    // default_ircserver: '127.0.0.1',
+    // default_ircchannel: '#your_chan'
 }
 
 exports.misc = {
