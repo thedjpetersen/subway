d2f4073c2552d0abc208b2e9ce5b5dd16448cc29
David Petersen <thedjpetersen@gmail.com>
2013-01-07 10:29:07 -0800

    Syntax cleanup

--- assets/js/views/chat_application.js
+++ assets/js/views/chat_application.js
@@ -41,9 +41,9 @@ var ChatApplicationView = Backbone.View.extend({
   render: function() {
     $('body').html($(this.el).html(ich.chat_application()));
     if (!irc.connected) {
-      this.overview = new OverviewView;
+      this.overview = new OverviewView();
     } else {
-      this.channelList = new ChannelListView;
+      this.channelList = new ChannelListView();
     }
     return this;
   },
@@ -70,18 +70,20 @@ var ChatApplicationView = Backbone.View.extend({
   // Show number of unread mentions in title
   showUnread: function() {
     var unreads = irc.chatWindows.unreadCount();
-    if (unreads > 0)
+    if (unreads > 0) {
       document.title = '(' + unreads + ') ' + this.originalTitle;
       if( window.unity.connected ) {
         window.unity.api.Launcher.setCount(unreads);
         window.unity.api.Launcher.setUrgent(true);
       }
-    else
+    }
+    else {
       document.title = this.originalTitle;
       if( window.unity.connected ) {
         window.unity.api.Launcher.clearCount();
         window.unity.api.Launcher.setUrgent(false);
       }
+    }
   },
 
   playSound: function(type) {
